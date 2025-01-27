0840f0031cbe35780082f6965c6b73be38441160
JIMIN LEE <jimin1222@naver.com>
2025-01-28 01:51:25 +0900

    mayobe commit

---
+++ src/main/java/com/mayo/mayobe/MayobeApplication.java
@@ -0,0 +1,13 @@
+package com.mayo.mayobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayobeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mayo/mayobe/MayobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayo.mayobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
